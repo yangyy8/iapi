5e779a034514f059287c772ede2b3559402a3528
aki2333 <aki_2333@163.com>
2018-07-27 09:22:02 +0800

    aki 23333

--- src/api/index.js
+++ src/api/index.js
@@ -7,12 +7,12 @@
 
 //var root="http://192.168.99.251:8080"    //时达的电脑
 
-//var root="http://192.168.99.242:8080"    //石飞的电脑
+var root="http://192.168.99.242:8080"    //石飞的电脑
 
 // var root = "http://192.168.99.251:8080"    //性能监控
 
 //var root="http://192.168.99.206:8080"   //服务器电脑
-var root=""
+//var root=""
 
 // var root = 'http://192.168.99.245:8080'
 //var root=""
@@ -76,7 +76,7 @@ function apiAxios(method, url, params, success, failure) {
       data: method === 'POST' || method === 'PUT' ? params : null,
       params: method === 'GET' || method === 'DELETE' ? params : null,
       baseURL: root,
-      withCredentials: false
+      withCredentials: true
 
     })
     .then(function(res) {
