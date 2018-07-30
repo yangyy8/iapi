8e472ada98f5af5abe4f3d04b38d9da9c74259c9
aki2333 <aki_2333@163.com>
2018-07-30 19:07:33 +0800

    aki 23333

--- src/api/index.js
+++ src/api/index.js
@@ -6,12 +6,6 @@
 //var root="http://192.168.99.248:8181"    //李志鹏的电脑
 
 
-
-// var root="http://192.168.99.251:8080"    //时达的电脑
-
-//var root="http://192.168.99.251:8080"    //时达的电脑
-
-
 // var root="http://192.168.99.251:8080"    //时达的电脑
 
 
@@ -19,42 +13,21 @@
 
 // var root = "http://192.168.99.251:8080"    //性能监控
 
-
-
-
-//var root="http://192.168.99.206:8080"   //服务器电脑
-
-//var root = 'http://192.168.99.245:8080'
-// var root=""
-
-//var root="http://192.168.99.206:8080"   //服务器电脑
-
 var root="http://192.168.99.206:8080"   //服务器电脑
 //var root=""
 
 // var root = 'http://192.168.99.245:8080'
-// var root=""
-
-// var root = 'http://192.168.99.245:8080'
-// var root=""
-//var root="http://192.168.99.206:8080"   //服务器电脑
-
 
 //var root="http://192.168.99.228:8080"    //丁勇的电脑
 
-// var root="http://192.168.99.206:8080"   //服务器电脑
-
-
 //var root="http://192.168.99.201:8080"    //全国监控
-
-
-//var root="http://192.168.99.201:8080"    //全国监控
-// var root="http://192.168.99.250:8080"    //刘洋的电脑
+//var root="http://192.168.99.250:8080"    //刘洋的电脑
 
 // 引用axios
 var axios = require('axios')
 import { Loading } from 'element-ui';
 import { Message } from 'element-ui';
+import { MessageBox } from 'element-ui';
 // 自定义判断元素类型JS
 function toType(obj) {
   return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
@@ -78,15 +51,21 @@ function filterNull(o) {
 
 // 添加默认参数
 
-
-
 function apiAxios(method, url, params, success, failure) {
-  // console.log(Loading)
   // let loadingInstance1 = Loading.service({ fullscreen: true });
+
   if (params) {
     // params=addParams(params);
     // params = filterNull(params);
   }
+  axios.post(root+'/manage-platform/isLanding',{},
+   r => {
+     console.log(r);
+     if(!r.data){
+       return
+     }
+  })
+
   axios({
       method: method,
       url: url,
