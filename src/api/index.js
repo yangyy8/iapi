762c602aefe7fad369b4bdcd012dda6dc650b93d
yangyy8 <yangsnow01@163.com>
2019-02-18 17:46:05 +0800

    yy

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
 // 配置API接口地址
 
- var root="http://192.168.99.206:8080"
+ // var root="http://192.168.99.206:8080"
 // var root=""
 
 
 
-// var root="http://192.168.99.234:8080"
+var root="http://192.168.99.234:8080"
 
 //var root="http://192.168.99.242:8081"
-// var root = 'http://192.168.99.248:8081'
+// var root ='http://192.168.99.248:8081'
 // var root="http://192.168.99.247:8080"
-//var root = 'http://192.168.99.244:8080'
+//var root ='http://192.168.99.244:8080'
 // var root="http://192.168.99.213:8080"   //服务器电脑
 // var root="http://192.168.99.228:8080"
 // var root="http://10.6.126.138:8088" //正式环境
@@ -39,7 +39,6 @@ function filterNull(o) {
   }
   return o
 }
-
 function isLOGIN(){
   let a;
   axios({
@@ -60,13 +59,11 @@ function isLOGIN(){
         window.location.reload();
         return;
       }})
-
     }
   })
   .catch(function (error) {
     console.log(error);
   });
-
 }
 function apiAxios(method, url, params, success, failure,header,responseType) {
   if(!(url=='/manage-platform/isLanding'||url=='/manage-platform/homePage/iapiSize'||url=='/manage-platform/landing')){
@@ -81,12 +78,10 @@ function apiAxios(method, url, params, success, failure,header,responseType) {
   //     return;
   //   }
   // }
-
   let loadingInstance1=null;
   if(!(url=='/manage-platform/nationwide/getPortDetail'||url=='/manage-platform/nationwide/getFlightDetail'||url=='/manage-platform/nameList/getNameListDataAnalysis'||url=='/manage-platform/riskRecordExtInterfaceController/getRecordOtherInfo'||url=='/manage-platform/iapiHead/queryListPageCount')){
     loadingInstance1 = Loading.service({ fullscreen: true, spinner: 'el-icon-loading',text:'拼命加载中',background:'rgba(0,0,0,0.6)',customClass:'loadingClass'});
   }
-
   axios({
       method: method,
       url: url,
@@ -119,8 +114,7 @@ function apiAxios(method, url, params, success, failure,header,responseType) {
                 Message.error(res.data.message);
               }
             }
-
-              success(res.data)
+        success(res.data)
         }
       } else {
         if (failure) {
