c940da47ee547cf05448c067b9f8a71abc80cbae
aki2333 <aki_2333@163.com>
2019-02-26 14:48:06 +0800

    aki

--- src/components/RiskAssessment/Management/DictionaryManagement.vue
+++ src/components/RiskAssessment/Management/DictionaryManagement.vue
@@ -35,7 +35,7 @@
     </div>
     <div class="middle" @mouseover="mouseHeader">
       <el-row class="mb-15">
-        <el-button type="primary" size="small" @click="adds(0,'');form={};">新增</el-button>
+        <el-button type="primary" id="DictionaryManagement_add" size="small" @click="adds(0,'');form={};">新增</el-button>
         <el-button type="success" size="small" @click="download">模板下载</el-button>
         <el-button type="info" size="small" @click="batchsdelete">批量删除</el-button>
         </el-row>
@@ -44,7 +44,7 @@
         border
         style="width: 100%;"
         class="mt-10 o-table3"
-@header-click="headerClick"
+        @header-click="headerClick"
         @selection-change="handleSelectionChange">
         <el-table-column
          type="selection"
@@ -74,7 +74,7 @@
         <el-table-column
           label="操作" width="150">
           <template slot-scope="scope">
-              <el-button type="text" class="a-btn" title="编辑"  icon="el-icon-edit" @click="adds(1,scope.row)"></el-button>
+              <el-button id="DictionaryManagement_edit" type="text" class="a-btn" title="编辑"  icon="el-icon-edit" @click="adds(1,scope.row)"></el-button>
               <el-button type="text" class="a-btn" title="导入"  icon="el-icon-upload2" @click="showUpload(scope.row)"></el-button>
               <el-button type="text" class="a-btn" title="导出"   icon="el-icon-download" @click="tableDown(scope.row)"></el-button>
               <el-button type="text" class="a-btn" title="删除"   icon="el-icon-delete" @click="deletes(scope.row)"></el-button>
